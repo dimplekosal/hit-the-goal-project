e0f340ee464c1026b0345cd488327b5bed30cb96
dimplekosal <129039138+dimplekosal@users.noreply.github.com>
2023-05-17 20:00:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var canvas= new fabric.canvas('myCanvas');
+var canvas= new fabric.Canvas('myCanvas');
 
 ball_y=0;
 ball_x=0;
@@ -11,12 +11,12 @@ block_image_width = 5;
 block_image_height = 5;
 
 function load_img(){
-	fabric.IMAGE.fromURL("golf-h1.png", function(Img) {
+	fabric.Image.fromURL("golf-h.png", function(Img) {
 		hole_obj = Img;
 hole_obj.scaleToWidth(50);
 hole_obj.scaleToHeight(50);
 hole_obj.set({
-	top:hole_y
+	top:hole_y,
 	left:hole_x
 });
 canvas.add(hole_obj);
@@ -27,18 +27,18 @@ canvas.add(hole_obj);
 
 function new_image()
 {
-	fabric.IMAGE.fromURL("ball.png", function(Img) {
-		hole_obj = Img;
+	fabric.Image.fromURL("ball.png", function(Img) {
+		ball_obj = Img;
 ball_obj.scaleToWidth(50);
 ball_obj.scaleToHeight(50);
 ball_obj.set({
-	top:ball_y
+	top:ball_y,
 	left:ball_x
 });
 canvas.add(ball_obj);
 });
 	
-	new_image();
+	
 }
 
 
@@ -49,9 +49,12 @@ function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	if( ball_x==hole_x)&&(ball_y==hole_y){
+	if  ((ball_x==hole_x) && ( ball_y==hole_y)){
 	canvas.remove(ball_obj);
 
+	document.getElementById("hd3").innerHTML="you have hit the goal!!!";
+document.getElementById("myCanvas").style.borderColor="red";
+	}  
 	else{
 		if(keyPressed == '38')
 		{
@@ -78,7 +81,7 @@ function my_keydown(e)
 	function up()
 	{
 		if (ball_y >= 0) {
-			ball_y = mall_y - block_image_height;
+			ball_y = ball_y - block_image_height;
 			canvas.remove(ball_obj);
 			new_image();
 		}
@@ -86,34 +89,34 @@ function my_keydown(e)
 
 	function down()
 	{
-		if (ball_y >= 0) {
-			ball_x = mall_x + block_image_width;
+		if (ball_y<=450) {
+			ball_y = ball_y + block_image_height;
 			canvas.remove(ball_obj);
 			new_image();
 	}
-
+	}
 	function left()
 	{
-		if(ball_x >5)
+		if(ball_x >=5)
 		{
-			if (ball_y >= 0) {
-				ball_y = mall_y - block_image_height;
+			 
+				ball_x = ball_x - block_image_width;
 				canvas.remove(ball_obj);
 				new_image();
 		}
-		}
+		
 	}
 
 	function right()
 	{
 		if(ball_x <=1050)
 		{
-			if (ball_y >= 0) {
+		
 				ball_x = ball_x + block_image_width;
 				canvas.remove(ball_obj);
 				new_image();
 		}
-		}
+		
 	}
 	
 }
